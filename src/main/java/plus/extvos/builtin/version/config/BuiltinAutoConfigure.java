7509602e5c253bf5896f529e25fa7bdd62e6b965
Mingcai SHEN <archsh@gmail.com>
2021-07-13 09:08:51 +0800

    Move base result process to lib-common from lib-restlet.

--- src/main/java/plus/extvos/builtin/version/config/BuiltinAutoConfigure.java
+++ src/main/java/plus/extvos/builtin/version/config/BuiltinAutoConfigure.java
@@ -1,6 +1,5 @@
 package plus.extvos.builtin.version.config;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -14,7 +13,6 @@ import springfox.documentation.spring.web.plugins.Docket;
  * @author Mingcai SHEN
  */
 @EntityScan("plus.extvos.builtin.entity")
-@MapperScan("plus.extvos.builtin.mapper")
 @ComponentScan(basePackages = "plus.extvos.builtin")
 public class BuiltinAutoConfigure {
     @Bean
